eed205ed7b5bcae11459e6517a1d667c03be34ff
@lassoued.skanderr <lassoued.skanderr@gmail.com>
2024-02-23 22:44:17 +0100

    1st

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import Layout from '../componets/Layout'
-import '../styles/globals.css'
-
+import Layout from "../componets/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
+/**
+ * Hello Rostem
+ */
